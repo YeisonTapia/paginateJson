2eebbefbc1b2365950cad6b14b75d082879809e4
Wilmer Ramiro Cristancho Cruz <wilmerc@talentu.co>
2020-02-23 14:13:47 -0500

    filter functionality is added

--- index.js
+++ index.js
@@ -1,26 +1,48 @@
-exports.paginate = function (array, page, perPage) {
-  let obj = {}
-  let start = (page - 1) * perPage
-  let end = page * perPage
+exports.paginate = function(array, page, perPage, dataItem, jsonData) {
+    let obj = {}
+    let start = (page - 1) * perPage
+    let end = page * perPage
 
-  obj.items = [...array].slice(start, end)
-  if (obj.items.length === 0) {
+    if (dataItem !== undefined && jsonData !== undefined) {
+        array = validateData(array, jsonData, dataItem);
+    }
+
+    obj.items = [...array].slice(start, end)
+    if (obj.items.length === 0) {
+        return obj
+    }
+
+    if (page > 1) {
+        obj.prev = page - 1
+    }
+
+    if (end < array.length) {
+        obj.next = page + 1
+    }
+
+    if (obj.items.length !== array.length) {
+        obj.current = page
+        obj.first = 1
+        obj.last = Math.ceil(array.length / perPage)
+    }
+
     return obj
-  }
+}
 
-  if (page > 1) {
-    obj.prev = page - 1
-  }
+function validateData(array, jsonData, dataItem) {
+    return array.filter(
+        function(data) {
+            if (typeof jsonData === 'string' && isNaN(jsonData) && isNaN(data[dataItem])) {
+                return data[dataItem].toLowerCase() == jsonData.toLowerCase()
+            } else {
+                return data[dataItem] == parseInt(jsonData)
+            }
 
-  if (end < array.length) {
-    obj.next = page + 1
-  }
+            if (typeof jsonData === 'number') {
 
-  if (obj.items.length !== array.length) {
-    obj.current = page
-    obj.first = 1
-    obj.last = Math.ceil(array.length / perPage)
-  }
+                return data[dataItem] == jsonData
+            }
 
-  return obj
+        }
+    );
 }
